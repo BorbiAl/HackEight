2ae08bedd18412e8767197a25c9a175c9fb65b79
BorisAl <boris.v.aleksandrov.2023@elsys-bg.org>
2024-03-14 23:24:58 +0200

    mb
    greshen save

--- script.js
+++ script.js
@@ -12,22 +12,6 @@ login.addEventListener("click", () => {
     formSection.classList.remove("form-section-move");
 });
 let visibilityButtons = document.querySelectorAll(".visibility-button");
-function myFunction() {
-  var x = document.getElementById("password ele");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
-function myFunction1() {
-  var x = document.getElementById("password1 ele");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
 const emailInput = document.getElementById("email");
 const emailError = document.getElementById("email-error");
 
