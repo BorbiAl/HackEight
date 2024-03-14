64ce7bcf95dd2a31f8a61ba4da4cc8cb86210932
BorisAl <boris.v.aleksandrov.2023@elsys-bg.org>
2024-03-14 23:20:45 +0200

    email validator
    added an email checker

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ login.addEventListener("click", () => {
     slider.classList.remove("moveslider");
     formSection.classList.remove("form-section-move");
 });
+let visibilityButtons = document.querySelectorAll(".visibility-button");
+function myFunction() {
+  var x = document.getElementById("password ele");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("password1 ele");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+const emailInput = document.getElementById("email");
+const emailError = document.getElementById("email-error");
+
+emailInput.addEventListener("input", () => {
+  const email = emailInput.value.trim();
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (regex.test(email)) {
+    emailError.textContent = "";
+  } else {
+    emailError.textContent = "Please enter a valid email address.";
+  }
+});
