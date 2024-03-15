4576e9073cbe68e9298287a6fc2f28ced8720ec4
VelinaTs <velina.l.tsaneva.2023@elsys-bg.org>
2024-03-15 17:19:59 +0200

    tests

---
+++ english.py
@@ -0,0 +1,60 @@
+from  flask import Flask
+answer1=input("antonim  of cold ")
+if answer1 == "hot":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'hot'")
+
+answer2=input("third forms of good ")
+if answer2 == "good, better, the best":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'good, better, the best'")
+
+answer3=input("homonym of blue ")
+if answer3 == "blew":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'blew'")
+
+answer4=input("definition of develop")
+if answer4 == "start to exist, experience, or possess" :
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'start to exist, experience, or possess'")
+
+answer5=input("word that have homograph ")
+if answer5 == "fine":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'fine'")
+
+answer6=input("definition of look after ")
+if answer6 == "take care of someone or something":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'take care of someone or something'")
+
+answer7=input("antonim of clean")
+if answer7 == "dirty":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'dirty'")
+
+answer8=input("homonym of to ")
+if answer8 == "too":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'too'")
+
+answer9=input(" word that have homograph ")
+if answer9 == "ring":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'ring'")
+
+answer10=input("third forms of take")
+if answer10 == "take, took, taken":
+    print("Верен отговор!")
+else:
+    print(" Грешен отговор! Отговорът е 'take, took, taken'")
