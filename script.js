7d1b8c78b474188436437b47aa60f0640f7c9e07
BorisAl <boris.v.aleksandrov.2023@elsys-bg.org>
2024-03-15 08:14:14 +0200

    Needed
    Направено, така че да не можеш да натиснеш sign-in/sign-up без да попълниш всички полета

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ login.addEventListener("click", () => {
     formSection.classList.remove("form-section-move");
 });
 let visibilityButtons = document.querySelectorAll(".visibility-button");
+function myFunction() {
+  var x = document.getElementById("password ele");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function myFunction1() {
+  var x = document.getElementById("password1 ele");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 const emailInput = document.getElementById("email");
 const emailError = document.getElementById("email-error");
 
@@ -25,3 +41,48 @@ emailInput.addEventListener("input", () => {
     emailError.textContent = "Please enter a valid email address.";
   }
 });
+
+emailInput.addEventListener("input", () => {
+  const email = emailInput.value.trim();
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (regex.test(email)) {
+    emailError.textContent = "";
+  } else {
+    emailError.textContent = "Please enter a valid email address.";
+  }
+});
+function checkform() {
+    var f = document.forms['theform'].elements;
+    var fieldsMustBeFilled = true;
+    var inputFields = Array.from(f).filter(field => field.type === 'text' || field.type === 'email' || field.type === 'password');
+
+    for (let i = 0; i < inputFields.length; i++) {
+        if (inputFields[i].value.length == 0)
+            fieldsMustBeFilled = false;
+    }
+
+    if (fieldsMustBeFilled) {
+        document.getElementById("form-btn").disabled = false;
+    }
+    else {
+        document.getElementById("form-btn").disabled = true;
+    }
+}
+function checkform1() {
+    var f1 = document.forms['theform1'].elements;
+    var fieldsMustBeFilled1 = true;
+    var inputFields1 = Array.from(f1).filter(field => field.type === 'text' || field.type === 'email' || field.type === 'password');
+
+    for (let i = 0; i < inputFields1.length; i++) {
+        if (inputFields1[i].value.length == 0)
+            fieldsMustBeFilled1 = false;
+    }
+
+    if (fieldsMustBeFilled1) {
+        document.getElementById("form-bt").disabled = false;
+    }
+    else {
+        document.getElementById("form-bt").disabled = true;
+    }
+}
